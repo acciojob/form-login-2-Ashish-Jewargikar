ad951cfb9d80e3b64cdfcf83140080001b044755
AshIsh <33283957+Ashish-Jewargikar@users.noreply.github.com>
2023-04-19 21:06:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function displayAlert(event) {
+  event.preventDefault(); // prevent form from submitting
+
+  const firstName = document.getElementById("firstName").value;
+  const lastName = document.getElementById("lastName").value;
+  const phoneNumber = document.getElementById("phoneNumber").value;
+  const emailId = document.getElementById("emailId").value;
+
+  alert(`First Name: ${firstName}\nLast Name: ${lastName}\nPhone Number: ${phoneNumber}\nEmail ID: ${emailId}`);
+}
+
+const loginForm = document.getElementById("loginForm");
+loginForm.addEventListener("submit", displayAlert);
